587980b310d13881ba511edb8b8517edde5f0021
David Moina <davemolina777@gmail.com>
2020-12-03 13:29:23 -0600

    Create a base structure for CLI

---
+++ lib/pokemon_pokedex.rb
@@ -0,0 +1,6 @@
+require "pokemon_pokedex/version"
+
+module PokemonPokedex
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pokemon_pokedex/version.rb
@@ -0,0 +1,3 @@
+module PokemonPokedex
+  VERSION = "0.1.0"
+end
