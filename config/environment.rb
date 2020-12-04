d2e6e1b3d654ffc42fde967269795a7e5d7b4a2b
David Moina <davemolina777@gmail.com>
2020-12-03 19:53:32 -0600

    Added an environment.rb in config folder and required all specific class files

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require "pry"
+#require api gems here
+
+require_relative '../lib/pokemon_pokedex/api'
+require_relative '../lib/pokemon_pokedex/cli'
+require_relative '../lib/pokemon_pokedex/pokemon'
+require_relative '../lib/pokemon_pokedex/team'
+#require_relative '../lib/pokemon_pokedex/version'
--- lib/pokemon_pokedex.rb
+++ lib/pokemon_pokedex.rb
@@ -4,3 +4,4 @@ module PokemonPokedex
   class Error < StandardError; end
   # Your code goes here...
 end
+require_relative '../config/environment'
