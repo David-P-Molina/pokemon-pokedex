07d22376fb798dcdbde2442d191e4afef814b91f
David Moina <davemolina777@gmail.com>
2020-12-04 11:18:02 -0600

    Added empty array and save method

--- lib/pokemon_pokedex/team.rb
+++ lib/pokemon_pokedex/team.rb
@@ -1,8 +1,14 @@
 require "pry"
 class Team
+    @@team = []
 #create an array with 6 spots
 #Stored in the array are user selected pokemon
-#method that adds to an array
+
+    def save#method that adds to an array
+        while @@team.length < 6
+            @@team << self
+        end
+    end
 #method that ensures there are only 6 spots filled
 #method that lists out the team
 #method that clears the team
