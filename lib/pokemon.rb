4aef139c08483dd5480c04680808f0315a50b7cd
David Moina <davemolina777@gmail.com>
2020-12-03 19:38:43 -0600

    Added pseudocode mothods to pokemon class

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,10 +2,15 @@ require "pry"
 class Pokemon
     #Methods to build
     # findbyname
+    # find or create by name
     # findbynumber
     # findbytype
     # initialize
-    
+    # self.all
+    # self.clear
+    # save
+    # self.create
+
 
 
 
