01071f2165315dbcb1582d430141896c4ebfe5f4
David Moina <davemolina777@gmail.com>
2020-12-04 11:07:45 -0600

    Created attr_accesor and initialize methods for pokemon class

--- lib/pokemon_pokedex/pokemon.rb
+++ lib/pokemon_pokedex/pokemon.rb
@@ -3,6 +3,17 @@ class Pokemon
     @@all = []
     #Methods to build
     # initialize
+    attr_accessor :name, :type, :secondarytype, :evolution, :description
+    attr_writer
+    attr_reader
+    def initialize(name, type, secondarytype = nil, evolution = nil, description)
+        @name = name
+        @type = type
+        @secondarytype = secondarytype
+        @evolution = evolution
+        @description = description
+        save
+    end
     def self.all    # self.all
         @@all
     end
