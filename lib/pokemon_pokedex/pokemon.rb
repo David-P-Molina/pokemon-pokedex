2980395371a2efccceecaeb0da5c596464c88d4c
David Moina <davemolina777@gmail.com>
2020-12-04 11:01:31 -0600

    Added .all, .clear, and save method to pokemon class

--- lib/pokemon_pokedex/pokemon.rb
+++ lib/pokemon_pokedex/pokemon.rb
@@ -1,18 +1,21 @@
 require "pry"
 class Pokemon
+    @@all = []
     #Methods to build
+    # initialize
+    def self.all    # self.all
+        @@all
+    end
+    def self.clear
+       @@all.clear 
+    end# self.clear is this needed?
+    def save# save
+        @@all << self
+    end
+    # self.create
     # findbyname
     # find or create by name
     # findbynumber
     # findbytype
-    # initialize
-    # self.all
-    # self.clear
-    # save
-    # self.create
-
-
-
-
-
+    
 end
