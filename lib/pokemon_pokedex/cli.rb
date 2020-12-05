e0b23c32d811a94d5d23fe11bff97341f8050e95
David Moina <davemolina777@gmail.com>
2020-12-04 18:52:09 -0600

    Added line method and pseudocoded functionality

--- lib/pokemon_pokedex/cli.rb
+++ lib/pokemon_pokedex/cli.rb
@@ -1,26 +1,37 @@
 require "pry"
 class CLI
     def start
+        line
         logo
+        line
         user_greeting
     end
     def poke_logo #Source https://ascii.co.uk/art/pokemon
-         puts "                                    .::."
-         puts "                                  .;:**' "           
-         puts "                         `                  "
-         puts "  .:XHHHHk.              db.   .;;.     dH  MX   "
-         puts "oMMMMMMMMMMM       ~MM  dMMP :MMMMMR   MMM  MR      ~MRMN"
-         puts "QMMMMMb  'MMX       MMMMMMP !MX' :M~   MMM MMM  .oo. XMMM 'MMM"
-         puts "  `MMMM.  )M> :X!Hk. MMMM   XMM.o'  .  MMMMMMM X?XMMM MMM>!MMP"
-         puts "   'MMMb.dM! XM M'?M MMMMMX.`MMMMMMMM~ MM MMM XM `''MX MMXXMM"
-         puts "    ~MMMMM~ XMM. .XM XM`'MMMb.~*?**~ .MMX M t MMbooMM XMMMMMP"
-         puts "     ?MMM>  YMMMMMM! MM   `?MMRb.    `'''   !L'MMMMM XM IMMM"
-         puts "      MMMX   'MMMM'  MM       ~%:           !Mh.''' dMI IMMP"
-         puts "      'MMM.                                             IMX"
-         puts "       ~M!M                                             IMP"
+         puts "                                       .::."
+         puts "                                     .;:**' "           
+         puts "                            `                  "
+         puts "     .:XHHHHk.              db.   .;;.     dH  MX   "
+         puts "   oMMMMMMMMMMM       ~MM  dMMP :MMMMMR   MMM  MR      ~MRMN"
+         puts "  'QMMMMMb  'MMX       MMMMMMP !MX' :M~   MMM MMM  .oo. XMMM 'MMM"
+         puts "     `MMMM.  )M> :X!Hk. MMMM   XMM.o'  .  MMMMMMM X?XMMM MMM>!MMP"
+         puts "      'MMMb.dM! XM M'?M MMMMMX.`MMMMMMMM~ MM MMM XM `''MX MMXXMM"
+         puts "       ~MMMMM~ XMM. .XM XM`'MMMb.~*?**~ .MMX M t MMbooMM XMMMMMP"
+         puts "        ?MMM>  YMMMMMM! MM   `?MMRb.    `'''   !L'MMMMM XM IMMM"
+         puts "         MMMX   'MMMM'  MM       ~%:           !Mh.''' dMI IMMP"
+         puts "         'MMM.                                             IMX"
+         puts "          ~M!M             Original 151 Pokemon            IMP"
 
     end
     def user_greeting
          puts "           Welcome to the Pokedex & Team Builder CLI app!"
     end
+    def line
+         puts "_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_"
+    end
+    #
+    #What pokemon would you like to learn about
+    #would you like to add this pokemon to your team?(Yes,No(I'll Keep looking))
+    #are you ready to start building your team?
+    #Are you sure you want to set your pokemon free?
+    #exit method "You are ready to face any obstacle! Now embark on your journey with your pokemon by your side!"
 end
