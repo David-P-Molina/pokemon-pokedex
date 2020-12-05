059dab41458aac19d0730c59f4f14faee4a63dad
David Moina <davemolina777@gmail.com>
2020-12-04 18:14:47 -0600

    Updated pokemon logo method and user_greeting method

--- lib/pokemon_pokedex/cli.rb
+++ lib/pokemon_pokedex/cli.rb
@@ -1,24 +1,26 @@
 require "pry"
 class CLI
     def start
+        logo
         user_greeting
     end
     def poke_logo #Source https://ascii.co.uk/art/pokemon
          puts "                                    .::."
-         puts "                                  .;:**'            
+         puts "                                  .;:**' "           
          puts "                         `                  "
          puts "  .:XHHHHk.              db.   .;;.     dH  MX   "
          puts "oMMMMMMMMMMM       ~MM  dMMP :MMMMMR   MMM  MR      ~MRMN"
-         puts "QMMMMMb  "MMX       MMMMMMP !MX' :M~   MMM MMM  .oo. XMMM 'MMM"
-         puts "  `MMMM.  )M> :X!Hk. MMMM   XMM.o"  .  MMMMMMM X?XMMM MMM>!MMP"
-         puts "   'MMMb.dM! XM M'?M MMMMMX.`MMMMMMMM~ MM MMM XM `" MX MMXXMM"
-         puts "    ~MMMMM~ XMM. .XM XM`"MMMb.~*?**~ .MMX M t MMbooMM XMMMMMP"
-         puts "     ?MMM>  YMMMMMM! MM   `?MMRb.    `"""   !L"MMMMM XM IMMM"
-         puts "      MMMX   "MMMM"  MM       ~%:           !Mh.""" dMI IMMP"
+         puts "QMMMMMb  'MMX       MMMMMMP !MX' :M~   MMM MMM  .oo. XMMM 'MMM"
+         puts "  `MMMM.  )M> :X!Hk. MMMM   XMM.o'  .  MMMMMMM X?XMMM MMM>!MMP"
+         puts "   'MMMb.dM! XM M'?M MMMMMX.`MMMMMMMM~ MM MMM XM `''MX MMXXMM"
+         puts "    ~MMMMM~ XMM. .XM XM`'MMMb.~*?**~ .MMX M t MMbooMM XMMMMMP"
+         puts "     ?MMM>  YMMMMMM! MM   `?MMRb.    `'''   !L'MMMMM XM IMMM"
+         puts "      MMMX   'MMMM'  MM       ~%:           !Mh.''' dMI IMMP"
          puts "      'MMM.                                             IMX"
          puts "       ~M!M                                             IMP"
-         puts ""
+
     end
-    def
-    puts "           Welcome to the Pokedex & Team Builder CLI app!"
+    def user_greeting
+         puts "           Welcome to the Pokedex & Team Builder CLI app!"
+    end
 end
