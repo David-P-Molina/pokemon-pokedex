0d91157e0a7c4aefe4de295eee8762e8ef86a29c
David Moina <davemolina777@gmail.com>
2020-12-07 16:38:36 -0600

    refactored pokemon_roster

--- lib/pokemon_pokedex/api.rb
+++ lib/pokemon_pokedex/api.rb
@@ -9,7 +9,7 @@ class API
     def get_pokemon
         uri = URI.parse(URL) #converts and parses out URL info
         response = Net::HTTP.get_response(uri) #uses builtin method to recieve a GET request that is a Net::HTTPOOK object
-        pokedex = JSON.parse(response.body)#Stores and transfers the nested data, the parse turns it into a hash or an array
+        response.body#Stores and transfers the nested data, the parse turns it into a hash or an array
        # ap pokedex #uses the gem awesome_print to format the output into something pretty and readable
         #pokemon.each do |poke|
     end
