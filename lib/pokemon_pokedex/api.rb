17ef6d205f21976e0ffa7ebb679c71804dda0980
David Moina <davemolina777@gmail.com>
2020-12-07 12:50:11 -0600

    Reviewed and added notes to understand what API is doing

--- lib/pokemon_pokedex/api.rb
+++ lib/pokemon_pokedex/api.rb
@@ -1,13 +1,15 @@
 require "pry"
 require "open-uri"
-require "json"
 require "net/http"
+require "json"
+require "awesome_print"
 class API
-    URL = 
-    def self.get_pokemon(URL)
-        uri-URI.parse(URL)
-        response = Net::HTTP.get_response(uri)
-        pokemon = JSON.parse(response.body)
+    URL = "https://pokeapi.co/api/v2/pokedex/2" #the main information
+    def self.get_pokemon()
+        uri = URI.parse(URL) #converts and parses out URL info
+        response = Net::HTTP.get_response(uri) #uses builtin method to recieve a GET request that is a Net::HTTPOOK object
+        pokedex = JSON.parse(response.body)#Stores and transfers the nested data, the parse turns it into a hash or an array
+       ap pokedex #uses the gem awesome_print to format the output into something pretty and readable
         #pokemon.each do |poke|
     end
 end
