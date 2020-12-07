626655e81704017530b52ab6ebd647eb6f3ea0df
David Moina <davemolina777@gmail.com>
2020-12-07 16:31:37 -0600

    Created method to parse information

--- lib/pokemon_pokedex/api.rb
+++ lib/pokemon_pokedex/api.rb
@@ -10,14 +10,25 @@ class API
         uri = URI.parse(URL) #converts and parses out URL info
         response = Net::HTTP.get_response(uri) #uses builtin method to recieve a GET request that is a Net::HTTPOOK object
         pokedex = JSON.parse(response.body)#Stores and transfers the nested data, the parse turns it into a hash or an array
-        ap pokedex #uses the gem awesome_print to format the output into something pretty and readable
+       # ap pokedex #uses the gem awesome_print to format the output into something pretty and readable
         #pokemon.each do |poke|
+    end
+    def get_pokemon_awesome
+        get_pokemon
+        ap pokedex
+    end
+    def pokemon_roster
+        pokemon_list = JSON.parse(self.get_pokemon)
+        pokemon_list.collect do |pokemon|
+            pokemon["name"]
+        end
     end
 
 end
-names = API.new.get_pokemon
-puts names
-
+# names = API.new.get_pokemon
+# puts names
+pokedex = API.new
+puts pokedex.pokemon_roster
 
 
 
