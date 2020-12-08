9fe90d833b50e3c56dc43eda18f7e4e68be5a5d0
David Moina <davemolina777@gmail.com>
2020-12-07 18:49:22 -0600

    Added user_select method to retrieve information pokemon

--- lib/pokemon_pokedex/api.rb
+++ lib/pokemon_pokedex/api.rb
@@ -4,10 +4,10 @@ require "net/http"
 require "json"
 require "awesome_print"
 class API
-    URL = "https://pokeapi.co/api/v2/pokemon?limit=151" #the main information/ 
     
     def get_pokemon
-        uri = URI.parse(URL) #converts and parses out URL info
+        url = "https://pokeapi.co/api/v2/pokemon?limit=151" #the main information/ 
+        uri = URI.parse(url) #converts and parses out URL info
         response = Net::HTTP.get_response(uri) #uses builtin method to recieve a GET request that is a Net::HTTPOOK object
         response.body#Stores and transfers the nested data, the parse turns it into a hash or an array
        # ap pokedex #uses the gem awesome_print to format the output into something pretty and readable
@@ -19,14 +19,18 @@ class API
     end
     def pokemon_roster
         pokemon_list = JSON.parse(self.get_pokemon)
-    #    binding.pry
-        pokemon_list['results'].collect do |hash| #hash is the first hash of a name and url
-      #      binding.pry
-            hash.each do |key, value|
-              p  "#{key}: #{value}"
-            end
+        list = pokemon_list['results'].collect do |hash| #hash is the first hash of a name and url
+        hash.each do |key, value|
+            p  "#{key}: #{value}"
         end
     end
+    def user_select(i)
+        input = "https://pokeapi.co/api/v2/pokemon-species/#{i}/"
+        uri = URI.parse(input) #converts and parses out URL info
+        response = Net::HTTP.get_response(uri) #uses builtin method to recieve a GET request that is a Net::HTTPOOK object
+        response.body
+    end
+end
 end
 # names = API.new.get_pokemon
 # puts names
