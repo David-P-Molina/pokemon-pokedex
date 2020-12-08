3271b7b641266b67a379a3f3fdb68e5cfc3a963b
David Moina <davemolina777@gmail.com>
2020-12-07 22:06:48 -0600

    Added all stat info to api created 7 methods

--- lib/pokemon_pokedex/api.rb
+++ lib/pokemon_pokedex/api.rb
@@ -20,13 +20,48 @@ class API
             end
         end
     end
-    # def select_stats#types, name, height, weight, stats 
-    #     binding.pry
-    #     url = "https://pokeapi.co/api/v2/pokemon/4/"
-    #     uri = URI.parse(url) #converts and parses out URL info
-    #     response = Net::HTTP.get_response(uri) #uses builtin method to recieve a GET request that is a Net::HTTPOOK object
-    #     response.body
-    # end
+
+
+    def select_stats#types, name, height, weight, stats 
+        binding.pry
+        url = "https://pokeapi.co/api/v2/pokemon/4/"
+        uri = URI.parse(url) #converts and parses out URL info
+        response = Net::HTTP.get_response(uri) #uses builtin method to recieve a GET request that is a Net::HTTPOOK object
+        response.body
+    end
+
+    def pokemon_stats
+        stat_list = JSON.parse(self.select_description)
+    end
+    def height
+        pokemon_stats
+        stat_list['weight']
+    end
+    def weight
+        pokemon_stats
+        stat_list['weight']
+    end
+    def type
+        pokemon_stats
+        stat_list['types'][0]['type']['name']
+    end
+    def name
+        pokemon_stats
+        stat_list['species']['name']
+    end
+    def stats
+        pokemon_stats #do i need to split this up?
+        # counter = 0
+        # while counter < 6
+        p stat_list['stats'][0]['stat']['name'] ":" stat_list['stats'][0]['base_stat']#hp
+        p stat_list['stats'][1]['stat']['name'] ":" stat_list['stats'][1]['base_stat']#attack
+        p stat_list['stats'][2]['stat']['name'] ":" stat_list['stats'][2]['base_stat']#defense
+        p stat_list['stats'][3]['stat']['name'] ":" stat_list['stats'][3]['base_stat']#special-attack
+        p stat_list['stats'][4]['stat']['name'] ":" stat_list['stats'][4]['base_stat']#special-defense
+        p stat_list['stats'][5]['stat']['name'] ":" stat_list['stats'][5]['base_stat']#speed
+    end
+
+
 
     def select_description #description ##figure out how to add userinput
         url = "https://pokeapi.co/api/v2/pokemon-species/1/" #use string interpolation
@@ -38,7 +73,6 @@ class API
         description_list = JSON.parse(self.select_description)
         description_list['flavor_text_entries'][0] #example"A strange seed was\nplanted on its\nback at birth.
                 #\fThe plant sprouts\nand grows with\nthis POKéMON." 
-        end
     end
 end
 # names = API.new.get_pokemon
