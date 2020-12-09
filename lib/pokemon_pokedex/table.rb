049f2d4618ae69885078b920cce4cd7830bc0f88
David Moina <davemolina777@gmail.com>
2020-12-09 09:51:01 -0600

    Created pokedex_display method

--- lib/pokemon_pokedex/table.rb
+++ lib/pokemon_pokedex/table.rb
@@ -1,12 +1,21 @@
 class Table
-    def display_card
+    def display_team_member()
         puts "╚═POKEDEX ##{number}| #{name}  "
         puts "╚═════════════════════════════╗"
         puts "╚═TYPE:#{type}"
-        puts "╚═HP: #{HP}    SPEED: #{speed}"
+        puts "╚═HP: #{hp}    SPEED: #{speed}"
         puts "╚═ATTACK: #{attack} SPC. ATTACK #{spc_attack}"
-        puts "╚═DEFENSE:#{attack} SPC. DEFENSE #{spc_defense}"
+        puts "╚═DEFENSE:#{defense} SPC. DEFENSE #{spc_defense}"
         puts "╚═HT: #{height}   WT: #{weight}"
         puts "╚═════════════════════════════╗"
+    end
+    def pokedex_display
+        puts "╚═POKEDEX ##{number}| #{name}  "
+        puts "╚═TYPE:#{type}"
+        puts "╚═════════════════════════════════════════════════════════════════════════════════════════════════════════╗"
+        puts "#{description}"
+        puts "╚═════════════════════════════════════════════════════════════════════════════════════════════════════════╝"
+        puts "╚═HP: #{hp}    SPEED: #{speed} ATTACK: #{attack} SPC. ATTACK #{spc_attack}"
+        puts "╚═DEFENSE:#{defense} SPC. DEFENSE #{spc_defense} HT: #{height}   WT: #{weight}"
     end
 end
