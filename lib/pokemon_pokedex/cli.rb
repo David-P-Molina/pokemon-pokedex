cc4d0525fe0a52722593a45cd718724c36907835
David Moina <davemolina777@gmail.com>
2020-12-11 08:09:15 -0600

    Added display card method to CLI

--- lib/pokemon_pokedex/cli.rb
+++ lib/pokemon_pokedex/cli.rb
@@ -174,5 +174,14 @@ class CLI
      def line_bottom
           puts "╚═════════════════════════════════════════════════════════════════════╝"
      end
+     def pokedex_display_card#accepts (number, name, type, description, hp, speed, attack, spc_attack, defense, spc_defense, height, weight)
+          puts "╚═POKEDEX ##{number}| #{name}  "
+          puts "╚═TYPE:#{type}"
+          puts "╚═════════════════════════════════════════════════════════════════════════════════════════════════════════╗"
+          puts "#{description}"
+          puts "╚═════════════════════════════════════════════════════════════════════════════════════════════════════════╝"
+          puts "╚═HP: #{hp}    SPEED: #{speed} ATTACK: #{attack} SPC. ATTACK #{spc_attack}"
+          puts "╚═DEFENSE:#{defense} SPC. DEFENSE #{spc_defense} HT: #{height}   WT: #{weight}"
+      end
 end
 ##tty for columns
