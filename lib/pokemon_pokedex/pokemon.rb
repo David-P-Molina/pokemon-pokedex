74919327047b0b1a3c2eef721546d7f4027f76b6
David Moina <davemolina777@gmail.com>
2020-12-12 19:23:01 -0600

    Removed old methods from pokemon and updated start file

--- lib/pokemon_pokedex/pokemon.rb
+++ lib/pokemon_pokedex/pokemon.rb
@@ -35,24 +35,12 @@ class Pokemon
         pokemon.save
         pokemon
     end
-
-
-    def get_pokemon_description#Uses second parse method to retrieve pokemon stats## add description retrieval
-      #  self.find_by_number
-        @description = description
-    end
     def self.find_by_number(number) #connect and use number and string interpolate it into url to search for info
         all.detect{|poke| poke.number == number}
         #pokemon.number
     end
-
-
     def self.find_by_url(url) 
         all.detect{|poke| poke.url == url}
     end
-    def get_pokemon_stats#Uses second parse method to retrieve pokemon stats## add description retrieval
-        pokemon_instance = self.find_url
-    end
-
 end
 
